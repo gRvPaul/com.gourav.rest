bf1c064150f77fff7100a0426bfc3adf314026ff
gRvPaul <gourav26061991@gmail.com>
2014-07-29 10:05:36 -0700

    3rd Commit
    Error removed

--- src/com/gourav/rest/V1_status.java
+++ src/com/gourav/rest/V1_status.java
@@ -5,22 +5,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/v1/status/*")
+@Path("/v1/status/")
 public class V1_status {
-	
+
 	private static final String api_version = "00.01.00";
-	
+
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String returnTitle(){
-		return "<p>JAVA Web Service</p>";
+	public String returnTitle() {
+		return "<p>JAVA WEB service</p>";
 	}
-	
-		@Path("/version")
-		@GET
-		@Produces(MediaType.TEXT_HTML)
-		public String returnVersion(){
-			return "<p>Version: </p>"+api_version;
+
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+		return "<p>Version: </p>" + api_version;
 	}
-	
 }
